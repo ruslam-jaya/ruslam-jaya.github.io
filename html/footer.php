be359ae25f799e4d8280b0ea1d1b04264e65f3cf
Maskibow <iszroil@gmail.com>
2022-03-07 15:37:07 +0700

    Add files via upload

---
+++ html/footer.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta name="robots" content="noindex,nofollow">
+        <meta name="googlebot" content="notranslate" />
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/font-awesome.min.css">
+        <link rel="stylesheet" href="../css/hero-slider.css">
+        <link rel="stylesheet" href="../css/templatemo-main.css">
+        <link rel="stylesheet" href="../css/owl-carousel.css">
+        <link rel="stylesheet" href="../css/navheader.css">
+	   	<link rel="stylesheet" href="../ccs/css?family=Open+Sans:300,400,600,700,800">
+       <script src="../js/jquery.mobile-1.4.5.min.js"></script>
+  	   <script src="../js/jquery.min.js"></script>
+       <script src="../js/opennav.js"></script>
+       <script src="../js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+	   <script src="../js/headbar.js"></script>
+       
+       
+</head>
+<body>
+    <footer id="contact-us">
+        <div class="container">
+            <div class="row" itemprop="address" itemscope itemtype="https://schema.org/PostalAddress">
+                <div class="col-md-4 col-sm-12">
+                    <div class="logo">
+                        <img alt='logo ruslam' src='../img/LOGO-TEXT-S.png' width="180px"/>
+                        <p>Copyright &copy; 2021 PT Ruslam Jaya
+                       <br>Design: Maskibow</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    <div class="location">
+                        <h4>Head Office Location</h4>
+                        <ul>
+                            <li itemprop="streetAddress">Jl. Cempaka Putih Tengah VI <br>No.4, RT.011/RW.005<br>Cempaka Putih Timur</li>
+                            <li itemprop="addressLocality">Cempaka Putih<br>Jakarta Pusat<br>DKI Jakarta</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-12" >
+                    <div class="contact-info" style="min-width:180px;">
+                        <h4>Contact</h4>
+                        <ul>
+                            <li itemprop="telephone"><em><i class="fa fa-phone"></i></em> : (021) 4204585</li>
+                            <li><em><i class="fa fa-envelope"></i></em> : cp-ruslam@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-12">
+                    <div class="connect-us">
+                        <h4>Social Media</h4>
+                        <ul>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
