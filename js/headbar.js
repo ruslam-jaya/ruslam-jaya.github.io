177f4b4085c57fd754ee935cf363d2292467ac75
Maskibow <iszroil@gmail.com>
2022-03-07 12:23:27 +0700

    Add files via upload

---
+++ js/headbar.js
@@ -0,0 +1,111 @@
+        /* Storing user's device details in a variable*/
+        let details = navigator.userAgent;
+  
+        /* Creating a regular expression 
+        containing some mobile devices keywords 
+        to search it in details string*/
+        let regexp = /android|iphone|kindle|ipad/i;
+  
+        /* Using test() method to search regexp in details
+        it returns boolean value*/
+        let isMobileDevice = regexp.test(details);
+  
+        if (isMobileDevice) {
+				function showhide1()
+				{
+					 document.getElementById("sh002").style.display = "none";
+					 document.getElementById("sh003").style.display = "none"; 
+					 var div = document.getElementById("sh001");  
+					 if (div.style.display !== "none") 
+					 {  
+						 div.style.display = "none";   
+					 }  
+					 else
+					 {  
+						 div.style.display = "block"; 
+					 } 
+				}
+				function showhide2()
+				{
+					 document.getElementById("sh001").style.display = "none";
+					 document.getElementById("sh003").style.display = "none"; 
+					 var div = document.getElementById("sh002");  
+					 if (div.style.display !== "none") 
+					 {  
+						 div.style.display = "none";   
+					 }  
+					 else
+					 {  
+						 div.style.display = "block"; 
+					 } 
+				} 
+				function showhide3()
+				{
+					 document.getElementById("sh001").style.display = "none";
+					 document.getElementById("sh002").style.display = "none";
+					 var div = document.getElementById("sh003");  
+					 if (div.style.display !== "none") 
+					 {  
+						 div.style.display = "none";   
+					 }  
+					 else
+					 {  
+						 div.style.display = "block"; 
+					 } 
+				}
+				function openNav() {
+					 var div = document.getElementById("navigate");  
+					 if (div.style.right !== "-380px") 
+					 {  
+						 div.style.right = "-380px";   
+					 }  
+					 else
+					 {  
+						 div.style.right = "0px"; 
+					 } 
+				}
+
+        } else {
+				function showhide1()
+				{
+					 document.getElementById("sh002").style.top = "-400px";
+					 document.getElementById("sh003").style.top = "-400px";  
+					 var div = document.getElementById("sh001");  
+					 if (div.style.top !== "-400px") 
+					 {  
+						 div.style.top = "-400px"; 
+					 }  
+					 else
+					 {  
+						div.style.top = "24px";
+					 }
+				}
+				function showhide2()
+				{
+					 document.getElementById("sh001").style.top = "-400px";
+					 document.getElementById("sh003").style.top = "-400px"; 
+					 var div = document.getElementById("sh002");  
+					 if (div.style.top !== "-400px") 
+					 {  
+						 div.style.top = "-400px";  
+					 }  
+					 else
+					 {  
+						 div.style.top = "24px";   
+					 } 
+				} 
+				function showhide3()
+				{
+					 document.getElementById("sh001").style.top = "-400px";
+					 document.getElementById("sh002").style.top = "-400px";
+					 var div = document.getElementById("sh003");  
+					 if (div.style.top !== "-400px") 
+					 {  
+						 div.style.top = "-400px";   
+					 }  
+					 else
+					 {  
+						 div.style.top = "24px"; 
+					 }  
+				}
+        }
---
+++ js/imagesnap.js
@@ -0,0 +1,5 @@
+var thumbnail_mode = 'yes';; //yes -with thumbnail, no -no thumbnail
+summary_noimg = 430; //summary length when no image
+summary_img = 340; //summary length when with image
+img_thumb_height = 100;
+img_thumb_width = 100;// JavaScript Document
