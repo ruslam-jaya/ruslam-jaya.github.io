a63441a53f066ca02be58eb6101accfa19a19f5e
Maskibow <iszroil@gmail.com>
2022-03-07 12:18:45 +0700

    Delete js directory

--- js/imagesnap.js
+++
@@ -1,5 +0,0 @@
-var thumbnail_mode = 'yes';; //yes -with thumbnail, no -no thumbnail
-summary_noimg = 430; //summary length when no image
-summary_img = 340; //summary length when with image
-img_thumb_height = 100;
-img_thumb_width = 100;// JavaScript Document
