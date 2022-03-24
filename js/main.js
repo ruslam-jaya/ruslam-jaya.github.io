d7acc717cecbdce7188ebadc3418a84b789bf9d6
Maskibow <iszroil@gmail.com>
2022-03-24 12:24:27 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,3 +50,17 @@ jQuery(document).ready(function($) {
 
 
 });
+
+$(document).ready(function(){
+$(".fixed-side-navbar a, .primary-button a").on('click', function(event) {
+if (this.hash !== "") {
+event.preventDefault();
+var hash = this.hash;
+$('html, body').animate({
+scrollTop: $(hash).offset().top
+}, 800, function(){
+window.location.hash = hash;
+});
+};
+});
+});
