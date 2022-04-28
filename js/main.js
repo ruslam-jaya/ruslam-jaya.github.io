7483a94358632b912f693670dcda0051877b4138
Maskibow <iszroil@gmail.com>
2022-04-28 17:10:48 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,3 +79,38 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+function socialWindow(url) {
+  var left = (screen.width - 570) / 2;
+  var top = (screen.height - 570) / 2;
+  var params = "menubar=no,toolbar=no,status=no,width=570,height=570,top=" + top + ",left=" + left;
+  // Setting 'params' to an empty string will launch
+  // content in a new tab or window rather than a pop-up.
+  // params = "";
+  window.open(url,"NewWindow",params);
+}
+
+function setShareLinks() {
+  var pageUrl = encodeURIComponent(document.URL);
+  var tweet = encodeURIComponent($("meta[property='og:description']").attr("content"));
+
+  $(".social-share.facebook").on("click", function() {
+    url = "https://www.facebook.com/sharer.php?u=" + pageUrl + tweet;
+    socialWindow(url);
+  });
+
+  $(".social-share.twitter").on("click", function() {
+    url = "https://twitter.com/share?url=" + pageUrl + "&text=" + tweet;
+    socialWindow(url);
+  });
+  
+  $(".social-share.ig").on("click", function() {
+    url = "https://www.instagram.com/?url=" + pageUrl + "&text=" + tweet;
+    socialWindow(url);
+  })
+  
+  $(".social-share.wa").on("click", function() {
+    url = "https://api.whatsapp.com/send?text==" + pageUrl + "/n" + "&text=" + tweet;
+    socialWindow(url);
+  })
+}
