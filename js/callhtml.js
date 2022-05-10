bebea5eaf710cec73a52fe4b025cb8e75e3c4f44
Maskibow <iszroil@gmail.com>
2022-05-10 16:54:20 +0700

    Update callhtml.js

--- js/callhtml.js
+++ js/callhtml.js
@@ -23,5 +23,17 @@
 			  /*exit the function:*/
 			  return;
 			}
-		  }
+		  };
+	
+                var url = "https://docs.google.com/spreadsheet/pub?key=1EDWsrhyhPpIl0sShstavd-P4oGmE1m2QCHcLKdExqOE&single=true&gid=1647130226&range=O2:O12&output=csv";
+          xmlhttp = new XMLHttpRequest();
+          xmlhttp.onreadystatechange = function () {
+              if (xmlhttp.readyState == 4) {
+                  document.getElementById("display").innerHTML = xmlhttp.responseText;
+                  //alert(xmlhttp.responseText);
+              }
+          };
+          xmlhttp.open("GET", url, true);
+          xmlhttp.send(null);
+      };
 		};
