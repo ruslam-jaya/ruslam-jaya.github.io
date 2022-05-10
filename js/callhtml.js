cfd570798e6d311eedbe2069f61044ce5279a2da
Maskibow <iszroil@gmail.com>
2022-05-10 17:17:50 +0700

    Update callhtml.js

--- js/callhtml.js
+++ js/callhtml.js
@@ -25,3 +25,15 @@
 			}
 		  }
 		};
+      function loadData() {
+          var url = "https://docs.google.com/spreadsheet/pub?key=1EDWsrhyhPpIl0sShstavd-P4oGmE1m2QCHcLKdExqOE&single=true&gid=1647130226&range=O2&output=csv";
+          xmlhttp = new XMLHttpRequest();
+          xmlhttp.onreadystatechange = function () {
+              if (xmlhttp.readyState == 4) {
+                  document.getElementById("display").innerHTML = xmlhttp.responseText;
+                  //alert(xmlhttp.responseText);
+              }
+          };
+          xmlhttp.open("GET", url, true);
+          xmlhttp.send(null);
+      }
